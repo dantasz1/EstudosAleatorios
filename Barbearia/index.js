b6e6f136f98dc039dfadce5621a3990bb98e1ee7
André <dantasluizandre@gmail.com>
2024-04-28 16:56:01 -0300

    estilo novo NavBar

--- Barbearia/index.js
+++ Barbearia/index.js
@@ -1,31 +1,3 @@
-//class NavBar {
-//
-//
-//    constructor(mobileMenu,textos , navLinks) {
-//        this.mobileMenu = document.querySelector(mobileMenu)
-//        this.textos = document.querySelector(textos)
-//        this.navLinks = document.querySelectorAll(navLinks)
-//        this.activeClass = "active"
-//    }
-//    addCLickEvent () {
-//        this.mobileMenu.addCLickEvent("click" , () => console.log("teste")
-//
-//        )}
-//    init() {
-//        if (this.mobileMenu) {
-//            this.addCLickEvent()
-//        }
-//        return this;
-//    }
-//}
-//
-// //let mobileNavBr = new MobileNavBr( 
-//".mobile-menu",
-//".textos",
-//".textos li",
-//
-//)
-//mobileNavBr.init()
 
 function menuShow() {
     let menuMobile = document.querySelector(".mobile-menu");
@@ -38,25 +10,6 @@ function menuShow() {
 
 
 
-
-//document.addEventListener('DOMContentLoaded', function () {
-//    const hamburgerButton = document.querySelector('.hamburger');
-//    const menu = document.querySelector('#menu');
-//
-//    // Adiciona um evento de clique ao ícone do hamburger
-//    hamburgerButton.addEventListener('click', function () {
-//        // Alterna a classe 'show' no menu para exibir/ocultar a barra lateral
-//        menu.classList.toggle('active');
-//    });
-//
-//    // Adiciona um evento de clique aos itens do menu para ocultar o menu ao clicar em um item
-//    const menuItems = menu.querySelectorAll('li');
-//    menuItems.forEach(function (item) {
-//        item.addEventListener('click', function () {
-//            menu.classList.remove('active'); // Oculta o menu ao clicar em um item
-//        });
-//    });
-//});
 
 
 
