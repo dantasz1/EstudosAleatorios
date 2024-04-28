633c6b9c2578531102151c1bd931961a32bce9b5
André <dantasluizandre@gmail.com>
2024-04-28 17:53:18 -0300

    iniciando o processo de slides

---
+++ Barbearia/JavaScript/slide.js
@@ -0,0 +1,19 @@
+let slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function previousSlide() {
+    showSlide(currentSlide - 1);
+}
+
+showSlide(currentSlide); // Exibindo o primeiro slide inicialmente
+
